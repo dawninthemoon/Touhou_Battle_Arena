4023ca5310aab33b3e0ccb472f8a2411e5a8f480
dawninthemoon <nuts1925@naver.com>
2023-11-14 22:24:49 +0900

    UniTask Imported

---
+++ Assets/Scripts/Commands/CommandBase.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+namespace Commands {
+    public interface IBattleCommand {
+        void Execute();
+    }
+
+    public class BattleCommand {
+        
+    }
+}
