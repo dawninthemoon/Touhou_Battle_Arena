718abdda1971c8f031ebbde88dbf7320deff30f1
dawninthemoon <nuts1925@naver.com>
2023-11-14 23:14:42 +0900

    UI Folder

--- Assets/Scripts/Commands/CommandBase.cs
+++ Assets/Scripts/Commands/CommandBase.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
+using Moves;
 
 namespace Commands {
     public interface IBattleCommand {
-        void Execute();
+        UniTaskVoid Execute(ExecutionArea executionArea, string variable);
     }
 
     public class BattleCommand {
