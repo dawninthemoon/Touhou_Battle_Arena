bd7bc9621c91447712dd3f8b2b096c783656183b
dawninthemoon <nuts1925@naver.com>
2023-11-24 21:30:05 +0900

    버튼을 막을 때 GO 버튼도 같이 막도록 변경

--- Assets/Scripts/Game/UI/MoveButtonControl.cs
+++ Assets/Scripts/Game/UI/MoveButtonControl.cs
@@ -13,6 +13,7 @@ public class MoveButtonControl : MonoBehaviour, ILoadable {
     [SerializeField] private SkillButton[] _skillButtons;
     [SerializeField] private Image[] _slotImages;
     [SerializeField] private Button _upArrow, _downArrow, _rightArrow, _leftArrow;
+    [SerializeField] private Button _decideButton;
     private Dictionary<string, Sprite> _skillIconDictionary;
     private static readonly string SkillIconLabel = "SkillIcons";
     public bool IsLoadCompleted {
@@ -30,6 +31,8 @@ public class MoveButtonControl : MonoBehaviour, ILoadable {
         _rightArrow.onClick.AddListener(() => MovementSelected(1));
         _downArrow.onClick.AddListener(() => MovementSelected(2));
         _leftArrow.onClick.AddListener(() => MovementSelected(3));
+
+        _decideButton.onClick.AddListener(() => _moveSlot.RequestExecuteAll());
     }
 
     private async UniTaskVoid Start() {
@@ -84,6 +87,7 @@ public class MoveButtonControl : MonoBehaviour, ILoadable {
         foreach (var skillButton in _skillButtons) {
             skillButton.SetInteraction(interactable);
         }
+        _decideButton.interactable = interactable;
         _upArrow.interactable = _downArrow.interactable = _leftArrow.interactable = _rightArrow.interactable
             = interactable;
     }
